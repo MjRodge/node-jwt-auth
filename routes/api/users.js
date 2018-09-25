28c91f9140fa028014dc4d4a5e6372f9acd56a13
Michael Rodgers <mail@mjrodge.me>
2018-09-25 15:05:56 -0400

    Added route to register user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,49 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+
+//load user model
+const User = require("../../models/User");
 
 // @route GET api/users/test
 // @desc Test user routes
 // @access Public
 router.get("/test", (req, res) => res.json({ message: "test for user" }));
+
+// @route GET api/users/register
+// @desc Register a user
+// @access Public
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already registered" });
+    } else {
+      //use gravatar for avatar data
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", //avatar size
+        r: "pg", //avatar rating
+        d: "mm" //default avatar
+      });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar: avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 //bring in routes
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 
 const app = express();
+
+//body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //DB config
 require("dotenv").config();
