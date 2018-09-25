91fd3519eef5dd5a73be2cb51f2cfb0424a277cf
Michael Rodgers <mail@mjrodge.me>
2018-09-25 14:40:28 -0400

    Added UserSchema

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+// @route GET api/users/test
+// @desc Test user routes
+// @access Public
 router.get("/test", (req, res) => res.json({ message: "test for user" }));
 
 module.exports = router;
