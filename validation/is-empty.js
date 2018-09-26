2a657c6dfd4f8456659d74dba791bd6b02fa8bbb
Michael Rodgers <mail@mjrodge.me>
2018-09-26 15:40:42 -0400

    Added server side validation for register route

---
+++ validation/is-empty.js
@@ -0,0 +1,9 @@
+//validator library only checks on strings, function expanded to check objects too
+
+const isEmpty = value =>
+  value === undefined ||
+  value === null ||
+  (typeof value === "object" && Object.keys(value).length === 0) ||
+  (typeof value === "string" && value.trim().length === 0);
+
+module.exports = isEmpty;
